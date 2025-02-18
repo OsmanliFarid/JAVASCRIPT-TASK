634ce96bd5e4114e0511039240223b7f4b6bccca
OsmanliFarid <osmanliferid686@gmail.com>
2025-02-18 23:12:54 +0400

    js change

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-let age = " "
-let bal = " "
+let age = ""
+let bal = ""
+
+prompt("nece yasin var")
+if ("" < 18) {
+    console.log("imtahana kece bilmezsiz 18 yasiniz olmuyub")
+}else{
+    console.log("imtahana gire bilersiz")
+}if(bal <= 80) {
+    console.log("imtahandan kecibsiz");
+    
+}
 
 
 
